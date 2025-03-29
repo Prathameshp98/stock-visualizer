46aa019bc38a4b594ed991d87dfb6bafd3ec5c13
Prathamesh Patil <prathameshpatil2812@gmail.com>
2025-03-29 17:20:29 +0530

    changed the external structure padding

--- src/StockVisualizer/StockVisualizer.tsx
+++ src/StockVisualizer/StockVisualizer.tsx
@@ -1,15 +1,19 @@
 
-import { PriceCard } from "./components";
+import { PriceCard, Tabs } from "./components";
+import styles from './StockVisualizer.module.css';
 
 const StockVisualizer = () => {
   return (
-    <div>
-      <PriceCard 
-        price={63197.71}
-        change={2161.42}
-        percentage={-3.54}
-        currency="USD"
-      />
+    <div className={styles.container}>
+        <PriceCard 
+            price={63197.71}
+            change={2161.42}
+            percentage={-3.54}
+            currency="USD"
+        />
+        <Tabs 
+            getSelectedTab={(tab) => console.log(tab)}
+        />
     </div>
   );
 }
